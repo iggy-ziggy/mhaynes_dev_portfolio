41823873c407c1d8da1d9c9b02b328de4072d574
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-08-12 15:59:07 -0400

    added responsive icon grid component

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,29 @@ import Button from "./components/Button/Button";
 import TextBlock from "./components/TextBlock/TextBlock";
 import Card from "./components/Card/Card";
 import Image from "./components/Image/Image";
+import IconGrid from "./components/IconGrid/IconGrid";
 import Placeholder from "./assets/generic_placeholder_500x500.png";
 import AboutMe from "./lib/AboutMe";
 import "./App.css";
 
 function App() {
+  const icons = [1,2,3,4,5,6,7,8,9,10,11,12]
   return (
     <>
-      <Card
-        title={`Hi. I'm Matt.`}
-        src={Placeholder}
-        alt="placeholder"
-        btnName="Contact Me"
-      >
-        <TextBlock>
-          <AboutMe />
-        </TextBlock>
-      </Card>
+    {/* <IconGrid>{icons}</IconGrid> */}
+      <div className="container">
+        <Card
+          title={`The Bag of Tricks`}
+          src={Placeholder}
+          alt="placeholder"
+          btnName="Resume"
+        >
+          <IconGrid>{icons}</IconGrid>
+          {/* <TextBlock>
+            <AboutMe />
+          </TextBlock> */}
+        </Card>
+      </div>
     </>
   );
 }
---
+++ src/components/IconGrid/IconGrid.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Placeholder from '../../assets/generic_placeholder_500x500.png'
+import "./IconGrid.css";
+
+const IconGrid = ({ children }) => {
+  return (
+    // <div className="icon_grid_container">
+    //   <div className="grid">
+    //     {children.map((c, i) => (
+    //       <div className="dummy_icon" key={i}>
+    //         {c}
+    //       </div>
+    //     ))}
+    //   </div>
+    // </div>
+    <div className="icon_grid_container">
+      <div className="grid">
+        {children.map((c, i) => (
+          <img className="dummy_icon" key={c} src={Placeholder} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// IconGrid.propTypes = {
+//     children: PropTypes.arrayOf(PropTypes.string)
+// }
+
+export default IconGrid;
