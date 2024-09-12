832b81a95b8c12feaa8d8ec7b4471c1dc40ac940
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-09-12 17:01:40 -0400

    added projects page and styles

--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -45,28 +45,9 @@ const projectArray = [
     github: "https://github.com/",
     deploy: "https://www.google.com/",
   },
-  {
-    title: "Project 9",
-    github: "https://github.com/",
-    deploy: "https://www.google.com/",
-  },
-  {
-    title: "Project 10",
-    github: "https://github.com/",
-    deploy: "https://www.google.com/",
-  },
 ];
 
 const ProjectsPage = () => {
-  const [rotations, setRotations] = useState([]);
-
-  useEffect(() => {
-    const initialRotations = projectArray.map(() => 
-      `${Math.random() > 0.5 ? "+" : "-"}${Math.random() * 15}deg`
-    );
-    setRotations(initialRotations);
-  }, []);
-
   return (
     <div>
       <Title text="The Goods" />
@@ -74,10 +55,7 @@ const ProjectsPage = () => {
         {projectArray.map((project, index) => (
           <div
             key={index}
-            className="project"
-            style={{
-              transform: `rotate(${rotations[index]})`,
-            }}
+            className={`project${index + 1}`}
           >
             <Project
               title={project.title}
@@ -90,5 +68,40 @@ const ProjectsPage = () => {
     </div>
   );
 };
+// const ProjectsPage = () => {
+//   const [rotations, setRotations] = useState([]);
+//   const [positions, setPositions] = useState([])
+
+//   useEffect(() => {
+//     const initialRotations = projectArray.map(() => 
+//       `${Math.random() > 0.5 ? "+" : "-"}${Math.random() * 15}deg`
+//     );
+//     setRotations(initialRotations);
+//   }, []);
+
+//   return (
+//     <div>
+//       <Title text="The Goods" />
+//       <div className="projects_container">
+//         {projectArray.map((project, index) => (
+//           <div
+//             key={index}
+//             className="project"
+//             style={{
+//               transform: `rotate(${rotations[index]})`,
+//               margin: ``
+//             }}
+//           >
+//             <Project
+//               title={project.title}
+//               github={project.github}
+//               deploy={project.deploy}
+//             />
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
 
 export default ProjectsPage;
