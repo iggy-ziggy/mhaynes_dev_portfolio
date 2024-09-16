49afdc585ff1d30f568bad87a1b9fa87068fecca
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-09-16 12:16:03 -0400

    added function to ensure title animation only runs when title is on screen

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
 import "./Title.css";
 
 const Title = ({ text, style }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const elementRef = useRef(null);
+
+  const handleScroll = () => {
+    if (elementRef.current) {
+      const rect = elementRef.current.getBoundingClientRect();
+      const windowHeight =
+        window.innerHeight || document.documentElement.clientHeight;
+
+      if (rect.top >= 0 && rect.top <= windowHeight * 0.9) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <h1 className="page_title" style={style}>
+    <h1
+      ref={elementRef}
+      className={`page_title ${isVisible ? "enlarge_title" : ""}`}
+      style={style}
+    >
       {text}
     </h1>
   );
