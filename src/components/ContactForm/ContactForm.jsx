6f5ada3612f4a4d96d7b34739b7943f74ffc6985
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-09-16 15:14:58 -0400

    added styles to contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -93,8 +93,8 @@ const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
 
 export default function Contact() {
   const [formData, setFormData] = useState({
-    name: "",
-    email: "",
+    from_name: "",
+    reply_to: "",
     message: "",
   });
 
@@ -104,7 +104,9 @@ export default function Contact() {
       ...formData,
       [name]: value,
     });
+    console.log(formData)
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY).then(
@@ -127,32 +129,45 @@ export default function Contact() {
     e.target.reset();
   };
   return (
-    <div className="contact--form">
-      <form onSubmit={handleSubmit}>
-        <div>
+    <div className="form_container">
+      <form className="contact_form" onSubmit={handleSubmit}>
+        <h3 className="form_header">Contact</h3>
+        <div className="input name_input">
           <label>Name</label>
           <input
+          id="from_name"
             type="text"
             name="from_name"
-            value={formData.name}
+            value={formData.from_name}
             onChange={handleChange}
+            onFocus={(event) => {
+              event.target.setAttribute("autocomplete", "off");
+            }}
+            // style={{color: 'var(--pink)', fontWeight: 'bold'}}
           />
         </div>
-        <div>
+        <div className="input email_input">
           <label>Email</label>
           <input
+          id="reply_to"
             type="email"
             name="reply_to"
-            value={formData.email}
+            value={formData.reply_to}
             onChange={handleChange}
+            onFocus={(event) => {
+              event.target.setAttribute("autocomplete", "off");
+            }}
+            // style={{color: 'var(--pink)', fontWeight: 'bold'}}
           />
         </div>
-        <div>
+        <div className="input message_input">
           <label>Message</label>
           <textarea
+          id="message"
             name="message"
             value={formData.message}
             onChange={handleChange}
+            style={{width: '100%'}}
           />
         </div>
         <Button type="submit" text="Submit" />
