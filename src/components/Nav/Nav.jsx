e62cc79642be3a94d5a163982661251e5cee6929
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-09-23 12:37:03 -0400

    added ability to close nav dropdown by clicking anywhere other than dropdown

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,21 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MenuIcon from "../../assets/MenuIcon.svg?react";
 import "./Nav.css";
 
 const Nav = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isVisible) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
 
   return (
     <div className="menu_icon_container">
       <div className="logo">MH</div>
-      <div className="dropdown" onClick={() => setIsVisible(!isVisible)}>
-        <MenuIcon className="menu_icon" />
-        <div className={`dropdown_list ${isVisible ? "visible" : ""}`}>
-          <a href="#skills_page">Skills</a>
-          <a href="#projects_page">Projects</a>
-          <a href="#contact_page">Contact</a>
-          <a href="#home_page">Home</a>
-        </div>
+      <div className="dropdown" onClick={toggleDropdown} ref={dropdownRef}>
+        <MenuIcon className={`menu_icon ${isVisible ? "open" : ""}`} />
+        <ul className={`dropdown_list ${isVisible ? "visible" : ""}`}>
+          <li>
+            <a href="#skills_page">Skills</a>
+          </li>
+          <li>
+            <a href="#projects_page">Projects</a>
+          </li>
+          <li>
+            <a href="#contact_page">Contact</a>
+          </li>
+          <li>
+            <a href="#home_page">Home</a>
+          </li>
+        </ul>
       </div>
     </div>
   );
