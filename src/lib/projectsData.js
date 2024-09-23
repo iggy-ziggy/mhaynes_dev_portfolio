2e214979f8fba74535541485a20ad7b02ad7959a
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-09-23 13:43:32 -0400

    added project data and minor style changes to projects page

---
+++ src/lib/projectsData.js
@@ -0,0 +1,54 @@
+const projects = [
+  {
+    title: "Artist Portfolio",
+    github: "https://github.com/iggy-ziggy/MHaynes_Art_Portfolio",
+    deploy: "https://mhaynes.netlify.app/",
+  },
+  {
+    title: "Game Hub",
+    github: "https://github.com/iggy-ziggy/game-hub",
+    deploy: "https://game-hub-wine-ten.vercel.app/",
+  },
+  {
+    title: "Cloud Hosting Front End",
+    github: "https://github.com/iggy-ziggy/cloud-hosting-front-end",
+    deploy: "https://cloud-hosting-front-end.netlify.app",
+  },
+  {
+    title: "inDev",
+    github: "https://github.com/iggy-ziggy/artwork-critique-app",
+    deploy: "https://hidden-crag-39805-685b6677aef9.herokuapp.com/",
+  },
+  {
+    title: "Power Grid",
+    github: "https://github.com/iggy-ziggy/PowerGrid",
+    deploy: "https://iggy-ziggy.github.io/PowerGrid/",
+  },
+  {
+    title: "Social Network API",
+    github: "https://github.com/iggy-ziggy/Social-Network-API",
+    deploy: "https://watch.screencastify.com/v/IUmMRwbsLSbKGZWOCFeT",
+  },
+  {
+    title: "E-Commerce Backend",
+    github: "https://github.com/iggy-ziggy/e-commerce-back-end",
+    deploy: "https://watch.screencastify.com/v/ugHBaT2o8CNyXhsXe0q0",
+  },
+  {
+    title: "Workday Scheduler",
+    github: "https://github.com/iggy-ziggy/work-day-scheduler",
+    deploy: "https://iggy-ziggy.github.io/work-day-scheduler/",
+  },
+  {
+    title: "Password Generator",
+    github: "https://github.com/iggy-ziggy/random-password-generator",
+    deploy: "https://iggy-ziggy.github.io/random-password-generator/",
+  },
+  {
+    title: "Code Quiz",
+    github: "https://github.com/iggy-ziggy/code_quiz",
+    deploy: "https://iggy-ziggy.github.io/code_quiz/",
+  },
+];
+
+export default projects;
--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -3,56 +3,14 @@ import "./ProjectsPage.css";
 import StickyNote from "../../assets/post_it.png";
 import Title from "../../components/Title/Title";
 import Project from "../../components/Project/Project";
-
-const projectArray = [
-  {
-    title: "Project 1",
-    github: "https://github.com/",
-    deploy: "https://www.google.com/",
-  },
-  {
-    title: "Project 2",
-    github: "https://github.com/",
-    deploy: "https://www.google.com/",
-  },
-  {
-    title: "Project 3",
-    github: "https://github.com/",
-    deploy: "https://www.google.com/",
-  },
-  {
-    title: "Project 4",
-    github: "https://github.com/",
-    deploy: "https://www.google.com/",
-  },
-  {
-    title: "Project 5",
-    github: "https://github.com/",
-    deploy: "https://www.google.com/",
-  },
-  {
-    title: "Project 6",
-    github: "https://github.com/",
-    deploy: "https://www.google.com/",
-  },
-  {
-    title: "Project 7",
-    github: "https://github.com/",
-    deploy: "https://www.google.com/",
-  },
-  {
-    title: "Project 8",
-    github: "https://github.com/",
-    deploy: "https://www.google.com/",
-  },
-];
+import projects from '../../lib/projectsData'
 
 const ProjectsPage = () => {
   return (
     <div id="projects_page" className="page_container">
       <Title text="The Goods" />
       <div className="projects_container">
-        {projectArray.map((project, index) => (
+        {projects.map((project, index) => (
           <div
             key={index}
             className={`project${index + 1}`}
@@ -68,40 +26,5 @@ const ProjectsPage = () => {
     </div>
   );
 };
-// const ProjectsPage = () => {
-//   const [rotations, setRotations] = useState([]);
-//   const [positions, setPositions] = useState([])
-
-//   useEffect(() => {
-//     const initialRotations = projectArray.map(() => 
-//       `${Math.random() > 0.5 ? "+" : "-"}${Math.random() * 15}deg`
-//     );
-//     setRotations(initialRotations);
-//   }, []);
-
-//   return (
-//     <div>
-//       <Title text="The Goods" />
-//       <div className="projects_container">
-//         {projectArray.map((project, index) => (
-//           <div
-//             key={index}
-//             className="project"
-//             style={{
-//               transform: `rotate(${rotations[index]})`,
-//               margin: ``
-//             }}
-//           >
-//             <Project
-//               title={project.title}
-//               github={project.github}
-//               deploy={project.deploy}
-//             />
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
 
 export default ProjectsPage;
