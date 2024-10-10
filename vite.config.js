5d8d36a7bd7d1f18b5254ce3658d4005c2045d7d
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-10-10 13:21:58 -0400

    added missing packages

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  build: {
-    rollupOptions: {
-      external: ['emailjs-com']
-    }
-  }
 });
